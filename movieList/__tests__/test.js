27fb870596f1a9cb63e908e7e0d16d7e51804a2b
Ethan Sharp <ethansharpdev@gmail.com>
2022-05-17 16:12:34 -0600

    movie list file set up for tests

---
+++ movieList/__tests__/test.js
@@ -0,0 +1,13 @@
+const {Builder, Capabilities, By} = require('selenium-webdriver')
+
+require('chromedriver')
+
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+beforeAll(async () => {
+    await driver.get('http://127.0.0.1:5500/movieList/index.html')
+})
+
+afterAll(async () => {
+    await driver.quit()
+})
